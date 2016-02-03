eb6f82cd457310aa74f723c26f60754635c85f91
Alex Jablonski <ajablonski@thoughtworks.com>
2016-02-02 20:00:18 -0500

    Replace DispatchActor with single publishActor

--- src/main/scala/DispatchService.scala
+++ src/main/scala/DispatchService.scala
@@ -1,31 +1,32 @@
 import java.util.UUID
 
 import DispatchActor._
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives
 import akka.stream.Materializer
+import akka.stream.actor.ActorSubscriberMessage
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.softwaremill.react.kafka.KafkaMessages.KafkaMessage
 import com.softwaremill.react.kafka.ReactiveKafka
 import conf.KafkaConfiguration
+import org.reactivestreams.Publisher
 
 class DispatchService(implicit fm: Materializer, system: ActorSystem) extends Directives {
   // Currently only one DispatchActor per service
   // This actor is what holds a reference to all the other actors subscribed
-  lazy val dispatchActor = system.actorOf(Props[DispatchActor])
-
-  // Wrapping our dispatchActor in a sink, all elements in stream will be sent to this actor
-  // We instruct the sink send an Unsubscribe message when the stream terminates
-  def dispatchInSink(sender: String) = Sink.actorRef[DispatchEvent](dispatchActor, Unsubscribe(sender))
+  val kafka: ReactiveKafka = new ReactiveKafka
+  val publishActor: ActorRef = kafka.producerActor(KafkaConfiguration.producerProperties)
 
   // Constructs and returns a flow that takes a received message, dispatches it to all subscribed actors
   def dispatchActorFlow(sender: String): Flow[ReceivedMessage, KafkaMessage[String], Unit] = {
-    val in = Flow[ReceivedMessage].to(dispatchInSink(sender))
+    val in: Sink[ReceivedMessage, Unit] = Flow[ReceivedMessage]
+      .map(elem => ActorSubscriberMessage.OnNext(elem.msg))
+      .to(Sink.actorRef(publishActor, ActorSubscriberMessage.OnComplete))
 
-    val kafka = new ReactiveKafka
-
-    val out: Source[KafkaMessage[String], Unit] = Source.fromPublisher(kafka.consume(KafkaConfiguration.consumerProperties("consumer" + UUID.randomUUID().toString)))
+    val consumerName: String = "consumer" + UUID.randomUUID().toString
+    val kafkaConsumer: Publisher[KafkaMessage[String]] = kafka.consume(KafkaConfiguration.consumerProperties(consumerName))
+    val out: Source[KafkaMessage[String], Unit] = Source.fromPublisher(kafkaConsumer)
     // When the flow is materialized for the first time (websocket connect) we create an actor for that flow
     // The newly created ActorRef is sent to our DispatchActor in a subscribe message so the DispatchActor will
     // broadcast new messages to this actor
